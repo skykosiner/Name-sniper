8babeab0f05330a60b44aa74faf81ccc05b096ba
Yoni Kosiner <yoni@kosiner.co.uk>
2020-07-23 20:01:27 +0100

    install commit

--- snipe.py
+++ snipe.py
@@ -1,34 +1,7 @@
-import os
-import time
-import random
-from bs4 import BeautifulSoup
-import requests
+import mojang_api
+import asyncio
 
-print("non of your login deltais will ever be stored")
-
-UserName=input("Enter you're minecraft username")
-Password=input("Enter you're minecraft password")
-
-print("Welcome to minecraft name sniper")
-
-print("What name would you like to snipe today")
-
-name=input()
-
-if (name == "360NOSCOPE"):
-     print("No that is my name bitch")
-
-print("starting the snipe")
-
-# Get the timer from name mc depending on the name
-source = requests.get("https://namemc.com/search?q=BlockBreaker6969")
-soup = BeautifulSoup(source, "lxml")
-html = soup.find("html")
-time = html.find("div", id="availability-time")
-
-
-print(time)
-
-
-
-
+async def main():
+    user = await Client.User.createUser('Minecraft playername')
+    await user.authenticate('Mojang Email', 'Mojang password')
+    await user.changeSkin('Skin url', slim_model = True)
